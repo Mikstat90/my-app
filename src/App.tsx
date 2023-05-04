53d03200b2a8388cdc4849d8dc538cc4c658a199
Dzmitry Tsitarau <mikstat90@gmail.com>
2023-05-04 21:55:59 +0200

    123

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,45 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    debugger
     return (
         <div>
             This is APP component
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
+
+function Rating() {
+    debugger
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    );
+}
+
+function Star() {
+    return (
+        <div>star</div>
+    );
+}
+
+function Accordion() {
+    debugger
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
         </div>
     );
 }
